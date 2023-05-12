7aca243e09bc1d3790d1fa26c9d1ace2fb1dae31
dongsam-E <dong2dong32@gmail.com>
2023-05-12 15:01:36 +0900

    웹기능사B형_강사님리뷰

---
+++ js/basic.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+
+
+})
+
+//알아둬야 하는 메서드
+// 주어용 메서드 :  eq(숫자) , find(자식객체) 
+// 동사용 메서드 : addClass(클래스명), removeClass(클래스명), toggleClass(클래스명)
+
+
+//알아둬야 하는 이벤트
+// ready : 선택자가 오직 하나 document
+// click : 모든 객체 해당됨
+// submit : 폼에 있는 button, input만 해당
+// change : 폼에 있는 input만 해당
+
+
+
+
